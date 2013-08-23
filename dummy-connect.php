c7caae197fe3f707c76b53c80866e3aed4d91d10
ando_masaki <ando_masaki@cygames.co.jp>
2013-08-23 18:49:55 +0900

    fsockopenに変更

--- dummy-connect.php
+++ dummy-connect.php
@@ -7,106 +7,19 @@ $default_param = array(
     "class"          => "Memcache",  // Memcache,MemcacheEx,Redis,Mysql
     "connect_method" => "addServer", // connect,pconnect,addServer,nonPaddServer
     "server"         => "localhost", // 接続サーバー localhost:11211 等のポート番号をつけることもできる
+    "port"           => "80",        // 接続ポート
     "close"          => "false",     // closeメソッドを呼ぶかどうか
     "pretty"         => "false",     // 結果をpretty print
-    "dummy_loop"     => "0",          // 空のコネクション数
+    "dummy_loop"     => "0",         // 空のコネクション数
+    "timeout"        => 60 * 60 * 24 // タイムアウト秒数
 );
+
 $option_params = (array) json_decode($_SERVER['argv'][1]);
 $param = (object) array_merge($default_param, $option_params);
-
-$cache = new Cache($param);
-$cache->connect();
-
-pcntl_signal(SIGTERM, array($cache, 'close'));
-pcntl_signal(SIGINT,  array($cache, 'close'));
-
-while(TRUE);
-
-class Cache
-{
-    private $param;
-    private $caches = array();
-
-    function __construct($param)
-    {
-        $this->param = $param;
-        for ($i = 0; $i < intval($this->param->dummy_loop); $i++)
-        {
-            $this->caches[] = new $this->param->class();
-        }
-    }
-
-    function connect()
-    {
-        $results = array();
-        foreach ($this->caches as $key => $cache)
-        {
-            $results[] = $cache->{$this->param->connect_method}($this->param->server);
-        }
-        if (in_array(FALSE, $results, TRUE)) {
-            die("Connect Error\n");
-        }
-        echo "Connect Success\n";
-    }
-
-    function close()
-    {
-        $results = array();
-        foreach ($this->caches as $cache)
-        {
-            $results[] = $cache->close();
-        }
-        if (in_array(FALSE, $results, TRUE)) {
-            die("Close Error\n");
-        }
-        echo "Close Success\n";
-        exit;
-    }
+$res = array();
+for($i = 1; $i <= $param->dummy_loop; $i++){
+    $res[] = fsockopen($param->server, $param->port);
+    usleep(10);
 }
-
-class MemcacheEx extends Memcache {
-    public function nonPaddServer($server){
-        $s = explode(":",$server);
-        $port = array_key_exists(2,$s)? $s[2]: 11211; 
-        return $this->addServer($server,$port,FALSE);
-    }
-}
-
-class Mysql {
-    private $db = "test";
-    private $user = "bmg";
-    private $pass = "hoge";
-    private $table = "test_innodb256";
-    
-    public function connect($server){
-        if( (mysql_connect($server,$this->user,$this->pass) === false) ||
-            (mysql_select_db($this->db) === false)
-          ){
-            return false;
-        }
-    }
-    public function pconnect(){
-        if( (mysql_pconnect($server,$this->user,$this->pass) === false) ||
-            (mysql_select_db($this->db) === false)
-          ){
-            return false;
-        }
-    }
-    public function close(){
-        return mysql_close();
-    }
-    public function set($key,$value){
-        return mysql_query("insert into {$this->table} values ('$key','$value')");
-    }
-    public function get($key){
-        $result = mysql_query("select value from {$this->table} where `key`='$key'");
-        if(!$result){
-            return $result;
-        }
-        else{
-            $row = (object) mysql_fetch_assoc($result);
-            return property_exists($row,'value')? $row->value : NULL;
-        }
-    }
-}
+sleep($param->timeout);
 
